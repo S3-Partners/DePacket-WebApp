7cbc3dc6cac03f26d46adf3765b20520430dfd0e
TaylorLi <mulhollanddrive2014@gmail.com>
2024-08-20 21:12:25 +0800

    RPW-8 mui (#2)
    * RPW-8 install MUI
    
    * RPW-8 mui icons
    
    * RPW-8 mui and layout
    
    ---------
    
    Co-authored-by: Taylor Li <258266864@qq.com>

---
+++ src/components/common/PageLayout/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { ReactElement, useState } from "react";
+import { usePathname } from "next/navigation";
+
+import classnames from "classnames";
+import css from "./styles.module.css";
+import Header from "@/components/common/Header";
+import LoadingError from "../LoadingError";
+import Footer from "../Footer";
+import { useIsSidebarRoute } from "@/hooks/useIsSidebarRoute";
+
+const PageLayout = ({ children }: { children: React.ReactNode }): ReactElement => {
+  const pathname = usePathname();
+  const [isSidebarRoute, isAnimated] = useIsSidebarRoute(pathname);
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(true);
+  const [isBatchOpen, setBatchOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <header className={css.header}>
+        <Header onMenuToggle={isSidebarRoute ? setSidebarOpen : undefined} onBatchToggle={setBatchOpen} />{" "}
+      </header>
+
+      {/* {isSidebarRoute && <SideDrawer isOpen={isSidebarOpen} onToggle={setSidebarOpen} />} */}
+
+      <div
+        className={classnames(css.main, {
+          [css.mainNoSidebar]: !isSidebarOpen || !isSidebarRoute,
+          [css.mainAnimated]: isSidebarRoute && isAnimated,
+        })}
+      >
+        <div className={css.content}>
+          <LoadingError>{children}</LoadingError>
+        </div>
+
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default PageLayout;
---
+++ src/utils/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2", // 你的主要颜色
+    },
+    secondary: {
+      main: "#dc004e", // 你的次要颜色
+    },
+  },
+  // 其他主题设置
+});
+
+export default theme;
