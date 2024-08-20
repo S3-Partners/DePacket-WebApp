01e9dc7708e8162089f4b98ec7b15784ec7416ec
gongjunqi <>
2024-08-20 15:48:10 +0800

    plan to  page

---
+++ src/app/receive/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Receive() {
+  return (
+    <>
+      <h1>plan to Receive</h1>
+    </>
+  );
+}
---
+++ src/app/send/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Send() {
+  return (
+    <>
+      <h1>plan to Send</h1>
+    </>
+  );
+}
